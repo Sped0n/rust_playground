f5b5d8a210cd757e0b154f5cb047d42e29797406
Sped0n <realsped0n@outlook.com>
2023-05-26 21:37:24 +0800

    feat(guessing_game): guessing game demo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+mod guessing_game;
 mod hello_world;
 
+#[allow(dead_code)]
 fn hello_world_practice() {
     use hello_world::{hello_world as hw, hello_world_alt as hwa};
     hw();
     hwa();
 }
 
+#[allow(dead_code)]
+fn guessing_game_practice() {
+    use guessing_game::guessing_game;
+    guessing_game();
+}
+
 fn main() {
-    hello_world_practice();
+    guessing_game_practice();
 }
