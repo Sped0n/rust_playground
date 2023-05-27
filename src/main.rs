d4c2c269d8c632e513d8e8b8a9d1c4f483ff56da
Sped0n <realsped0n@outlook.com>
2023-05-27 19:32:52 +0800

    feat(main): add matching case for different scenario

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
+use std::io;
+
+mod control_flow;
+mod formatted_print;
 mod guessing_game;
 mod hello_world;
 
-#[allow(dead_code)]
-fn hello_world_practice() {
-    use hello_world::{hello_world as hw, hello_world_alt as hwa};
-    hw();
-    hwa();
-}
-
-#[allow(dead_code)]
-fn guessing_game_practice() {
-    use guessing_game::guessing_game;
-    guessing_game();
-}
-
 fn main() {
-    guessing_game_practice();
+    loop {
+        let mut mode = String::new();
+        println!("Select a program to run:");
+        io::stdin()
+            .read_line(&mut mode)
+            .expect("Failed to read line");
+        match mode.trim() {
+            "hello_world" => {
+                hello_world::hello_world();
+                hello_world::hello_world_alt();
+            }
+            "guessing_game" => guessing_game::guessing_game(),
+            "std_fmt_general" => formatted_print::std_fmt_general(),
+            "std_fmt_display" => formatted_print::std_fmt_display(),
+            "std_fmt_list" => formatted_print::std_fmt_list(),
+            "std_fmt_formatting" => formatted_print::std_fmt_formatting(),
+            _ => {
+                println!("Invalid mode");
+                continue;
+            }
+        }
+        break;
+    }
 }
